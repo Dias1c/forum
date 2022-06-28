149c5181eb89c52574cbd09b775f14ccbcd07690
Dias1c <dias.kappasov@gmail.com>
2022-06-28 22:20:29 +0600

    fix: bug with conifgs

--- architecture/web/server/server.go
+++ architecture/web/server/server.go
@@ -9,9 +9,9 @@ import (
 
 type Configs struct {
 	Port           string `port`
-	ReadTimeout    int    `read_timeout`
-	WriteTimeout   int    `write_timeout`
-	IdleTimeout    int    `idle_timeout`
+	ReadTimeout    int    `read_timeout_ms`
+	WriteTimeout   int    `write_timeout_ms`
+	IdleTimeout    int    `idle_timeout_ms`
 	MaxHeaderBytes int    `max_header_bytes`
 }
 
@@ -24,9 +24,9 @@ func (s *Server) Run(configs *Configs, handler http.Handler) error {
 		Addr:           configs.Port,
 		Handler:        handler,
 		MaxHeaderBytes: configs.MaxHeaderBytes,
-		ReadTimeout:    time.Duration(configs.ReadTimeout),
-		WriteTimeout:   time.Duration(configs.WriteTimeout),
-		IdleTimeout:    time.Duration(configs.IdleTimeout),
+		ReadTimeout:    time.Duration(configs.ReadTimeout * int(time.Millisecond)),
+		WriteTimeout:   time.Duration(configs.WriteTimeout * int(time.Millisecond)),
+		IdleTimeout:    time.Duration(configs.IdleTimeout * int(time.Millisecond)),
 	}
 
 	log.Printf("Server runs on http://localhost%s\n", s.httpServer.Addr)
