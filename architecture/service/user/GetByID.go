6f0c0265f6cf83e970215ac8e8cd8eb26c85309e
Dias1c <dias.kappasov@gmail.com>
2022-07-09 17:54:10 +0600

    add: service

--- architecture/service/user/GetByID.go
+++ architecture/service/user/GetByID.go
@@ -1,7 +1,19 @@
 package user
 
-import model "forum/architecture/models"
+import (
+	"errors"
+	"fmt"
+	model "forum/architecture/models"
+	ruser "forum/architecture/repository/user"
+)
 
 func (u *UserService) GetByID(id int64) (*model.User, error) {
-	return nil, nil
+	usr, err := u.repo.GetByID(id)
+	switch {
+	case err == nil:
+		return usr, nil
+	case errors.Is(err, ruser.ErrNotFound):
+		return nil, ErrNotFound
+	}
+	return nil, fmt.Errorf("u.repo.GetByID: %w", err)
 }
