18532b0cf42774179a1681c7dc09e6b2d4913b37
Dias1c <dias.kappasov@gmail.com>
2022-08-01 21:30:12 +0600

    add: post-edit page

---
+++ architecture/web/handler/handler-post-edit.go
@@ -0,0 +1,71 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"forum/architecture/web/handler/view"
+	"log"
+	"net/http"
+	"strconv"
+
+	spost "forum/architecture/service/post"
+)
+
+// PostCreateHandler -
+func (m *MainHandler) PostEditHandler(w http.ResponseWriter, r *http.Request) {
+	debugLogHandler("PostEditHandler", r)
+
+	// Allowed Methods
+	switch r.Method {
+	case http.MethodGet:
+	case http.MethodPost:
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	iUserId := r.Context().Value("UserId")
+	if iUserId == nil {
+		log.Println("PostCreateHandler: r.Context().Value(\"UserId\") is nil")
+		pg := &view.Page{Error: fmt.Errorf("internal server error, maybe try again later")}
+		w.WriteHeader(http.StatusInternalServerError)
+		m.view.ExecuteTemplate(w, pg, "post-create.html")
+		return
+	}
+
+	userId := iUserId.(int64)
+	user, _ := m.service.User.GetByID(userId)
+
+	switch r.Method {
+	case http.MethodGet:
+		strPostId := r.URL.Query().Get("id")
+		postId, err := strconv.ParseInt(strPostId, 10, 64)
+		if err != nil || postId < 1 {
+			http.Error(w, "Invalid query id", http.StatusBadRequest)
+			return
+		}
+		post, err := m.service.Post.GetByID(postId)
+		switch {
+		case err == nil:
+		case errors.Is(err, spost.ErrNotFound):
+			// TODO: error page
+			http.Error(w, "Post Not Found", http.StatusNotFound)
+			return
+		}
+		categories, err := m.service.Category.GetByPostID(post.Id)
+		switch {
+		case err == nil:
+			post.WCategories = categories
+		default:
+			log.Printf("PostEditHandler: m.service.Session.GetByUuid: %v\n", err)
+			http.Error(w, "something wrong, maybe try again later", http.StatusInternalServerError)
+			return
+		}
+
+		pg := &view.Page{User: user, Post: post}
+		m.view.ExecuteTemplate(w, pg, "post-edit.html")
+		return
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
