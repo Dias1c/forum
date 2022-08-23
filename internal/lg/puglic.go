47a2a0c4fa6c04e15031a65a89096609a78f1fa3
Dias1c <dias.kappasov@gmail.com>
2022-08-23 11:54:49 +0600

    add: colorfull errors

--- internal/lg/puglic.go
+++ internal/lg/puglic.go
@@ -7,7 +7,7 @@ import (
 
 func init() {
 	Info = log.New(os.Stdout, "INFO  ", log.LstdFlags)
-	Err = log.New(os.Stdout, "ERROR ", log.LstdFlags)
+	Err = log.New(os.Stdout, "\u001b[31mERROR \u001b[0m", log.LstdFlags)
 }
 
 var (
