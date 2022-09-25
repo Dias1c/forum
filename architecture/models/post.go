89b949edcf7eced6cce9db05c293b2c2f32918b8
Dias1c <dias.kappasov@gmail.com>
2022-09-25 13:46:48 +0600

    add: get by user id method

--- architecture/models/post.go
+++ architecture/models/post.go
@@ -25,6 +25,7 @@ type IPostService interface {
 	Update(post *Post) error
 	GetAll(offset, limit int64) ([]*Post, error)
 	GetByID(id int64) (*Post, error)
+	GetByUserID(userId, offset, limit int64) ([]*Post, error)
 	DeleteByID(id int64) error
 }
 
@@ -33,5 +34,6 @@ type IPostRepo interface {
 	Update(post *Post) error
 	GetAll(offset, limit int64) ([]*Post, error)
 	GetByID(id int64) (*Post, error)
+	GetByUserID(userId, offset, limit int64) ([]*Post, error)
 	DeleteByID(id int64) error
 }
